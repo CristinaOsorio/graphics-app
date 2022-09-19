19dd22f83f6352656fd68a22f1094ae4cd75e9ae
CristinaOsorio <macry_08@hotmail.com>
2022-09-19 18:21:31 -0500

    CReacion de menu y routa de graph-http

--- src/app/graphics/graphics-routing.module.ts
+++ src/app/graphics/graphics-routing.module.ts
@@ -3,6 +3,7 @@ import { DoubleBargraphComponent } from './pages/double-bargraph/double-bargraph
 import { BargraphComponent } from './components/bargraph/bargraph.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PieGraphHttpComponent } from './pages/pie-graph-http/pie-graph-http.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,7 @@ const routes: Routes = [
       { path: 'bar', component: BargraphComponent },
       { path: 'double', component: DoubleBargraphComponent },
       { path: 'pie', component: PieGraphComponent, pathMatch: "full" },
+      { path: 'pie-http', component: PieGraphHttpComponent, pathMatch: "full" },
       { path: '**', redirectTo: 'bar' },
     ]
   }
--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  route: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styles: [
-  ]
+  styles: [`li {cursor: pointer}`]
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
+
+  menu: MenuItem[] = [
+    { route: '/graph/bar', text: 'Barra' },
+    { route: '/graph/double', text: 'Barra doble' },
+    { route: '/graph/pie', text: 'Circular' },
+    { route: '/graph/pie-http', text: 'Circular con Http' },
+  ];
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
