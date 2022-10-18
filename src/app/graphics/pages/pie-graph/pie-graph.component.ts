873ae0e7b318769240f866a416dbec90dfdbf8c6
CristinaOsorio <macry_08@hotmail.com>
2022-10-18 13:14:59 -0500

    ejemplo de grafica de dona

--- src/app/graphics/pages/pie-graph/pie-graph.component.ts
+++ src/app/graphics/pages/pie-graph/pie-graph.component.ts
@@ -1,3 +1,4 @@
+import { ChartType, ChartData, ChartEvent } from 'chart.js';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './pie-graph.component.html'
 })
 export class PieGraphComponent implements OnInit {
-
+ // Doughnut
+ public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+ public doughnutChartData: ChartData<'doughnut'> = {
+   labels: this.doughnutChartLabels,
+   datasets: [
+     { data: [ 350, 450, 100 ] },
+   ]
+ };
+ public doughnutChartType: ChartType = 'doughnut';
   constructor() { }
 
   ngOnInit(): void {
